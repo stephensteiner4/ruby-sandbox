8dd84b3bc37e192bc9359e765b7d666c0f7f3f01
Stephen Steiner <46692873+stephensteiner4@users.noreply.github.com>
2024-06-28 00:33:45 +0000

    first commit to sandbox, completing the "Running real Ruby programs" lesson

---
+++ howdy.rb
@@ -0,0 +1,13 @@
+### Comment here
+require "active_support/all"
+
+my_string = "Hello, world!"
+pp my_string
+
+require "./goodbye.rb"
+
+pp "What's your name?"
+
+their_name = gets.chomp
+
+puts "Hello, " + their_name + "!"
